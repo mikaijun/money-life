34d04af3bd9bf08292f0e34a5a41fd1a7fe36642
mikaijun <mikai@anyinc.jp>
2022-10-10 11:52:14 +0900

    Artifact Registryの作り直し

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,21 @@
-locals {
- backend_app_name  = "blog-training-backend-app"
- frontend_app_name = "blog-training-frontend-app"
+
+
+terraform {
+  required_version = ">= 1.0.0"
+  backend "gcs" {
+    prefix = "tfstate/v1"
+  }
+}
+
+## project ##
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.primary_region
 }
 
 # Cloud Run のデプロイで利用するArtifact Registry のリポジトリ
 module "artifact-registry" {
- source                     = "./modules/artifact-registry"
- gcp_project_id             = var.gcp_project_id
- artifact_registry_location = var.primary_region
- backend_app_name           = local.backend_app_name
- frontend_app_name          = local.frontend_app_name
+  source                     = "./modules/artifact-registry"
+  gcp_project_id             = var.gcp_project_id
+  artifact_registry_location = var.primary_region
 }
