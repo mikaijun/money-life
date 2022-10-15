2254e2833b91012e269d047a6c6802563910eea0
mikaijun <mikai@anyinc.jp>
2022-10-16 08:51:01 +0900

    envrcの使用をやめた

--- backend/prisma/migrations/20221015233331_init/migration.sql
+++ backend/prisma/migrations/20221015233331_init/migration.sql
@@ -17,6 +17,16 @@ CREATE TABLE "posts" (
     CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "posts_content_path_key" ON "posts"("content_path");
 
