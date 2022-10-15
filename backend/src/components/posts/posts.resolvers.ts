b59216b85154b5ae64d9cfe92e7ef7f1819a9787
mikaijun <mikai@anyinc.jp>
2022-10-15 14:42:10 +0900

    dbをsaveできるようにした

--- backend/src/components/posts/posts.resolvers.ts
+++ backend/src/components/posts/posts.resolvers.ts
@@ -1,26 +1,11 @@
 import { PrismaService } from './../prisma/prisma.service';
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PostModel } from './interfaces/posts.model';
 import { GetPostsArgs } from './interfaces/get-posts-connection.args';
 
 @Resolver(() => PostModel)
 export class PostsResolver {
   constructor(private readonly prisma: PrismaService) {}
-
-  // 役目を終えたのでリネーム
-  @Query(() => [PostModel], { name: 'fixedPosts', nullable: true })
-  async getPostsByFixedData() {
-    return [
-      {
-        id: '1',
-        title: 'NestJS is so good.',
-      },
-      {
-        id: '2',
-        title: 'GraphQL is so good.',
-      },
-    ];
-  }
 
   @Query(() => [PostModel], { name: 'prismaPosts', nullable: true })
   async getPostsByPrisma() {
@@ -42,5 +27,33 @@ export class PostsResolver {
         publishDate: 'desc',
       },
     });
+  }
+
+  @Mutation(() => PostModel)
+  async upvotePost(
+    @Args({ name: 'postId', type: () => String }) postId: string,
+  ) {
+    const data = {
+      id: 'fa119cb6-9135-57f5-8a5a-54f28d566d0e',
+      contentPath: '/storage/posts/articles/hello.md',
+      emoji: '✅',
+      excerpt: 'テスト追加',
+      md5Hash: '5ce6822c5efacf5791b7f46187451e73',
+      title: 'テストタイトル',
+      thumbNailUrl: 'http://exaample.com/image1.png',
+      type: 'article',
+      publishDate: new Date('2022-01-31'),
+      published: true,
+      like: 0,
+      createdAt: new Date('2022-01-31T04:34:22+09:00'),
+      updatedAt: new Date('2022-01-31T04:34:22+09:00'),
+    };
+    const post = await this.prisma.post.upsert({
+      where: { id: postId },
+      update: data,
+      create: data,
+    });
+
+    return post;
   }
 }
